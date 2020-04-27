79e7e36f8e832ca0ac4cf7e9502f3a3f444b2fff
3cmake <58825891+3cmake@users.noreply.github.com>
2020-04-27 22:24:34 +0700

    Updates.

--- main.ts
+++ main.ts
@@ -64,55 +64,56 @@ namespace CrocoKit {
         CH = 0x0080,
     }
 
-}
-//% blockId=CrocoKit_KeyBoard_Touch block="KeyBoard Touch Return|value %value"
-//% weight=94
-//% blockGap=20 
-//% color="#8FBC8F"
-//% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-export function KeyBoard_Touch(value: touch_pin): boolean {
-    let a = 0;
-    let b = 0;
-    let c = 0;
-    let temp = 0;
-    pins.i2cWriteNumber(0x50, 8, NumberFormat.UInt8BE, false);
-    a = pins.i2cReadNumber(0x50, NumberFormat.UInt8BE, true);
-    b = pins.i2cReadNumber(0x50, NumberFormat.UInt8BE, false);
-    c = (b << 8) | a;
-    if ((c & temp) != 0) {
-        c = c & temp;
-    } else {
-        switch (value) {
-            case touch_pin.None:
-                return (c & touch.None) == 0;
-                break;
-            case touch_pin.P2:
-                return (c & touch.C) == 0x0001;
-                break;
-            case touch_pin.P5:
-                return (c & touch.D) == 0x0002;
-                break;
-            case touch_pin.P8:
-                return (c & touch.E) == 0x0004;
-                break;
-            case touch_pin.P11:
-                return (c & touch.F) == 0x0008;
-                break;
-            case touch_pin.P12:
-                return (c & touch.G) == 0x0010;
-                break;
-            case touch_pin.P13:
-                return (c & touch.A) == 0x0020;
-                break;
-            case touch_pin.P14:
-                return (c & touch.B) == 0x0040;
-                break;
-            case touch_pin.P15:
-                return (c & touch.CH) == 0x0080;
-                break;
-            default:
-                break;
+    //% blockId=CrocoKit_KeyBoard_Touch block="KeyBoard Touch Return|value %value"
+    //% weight=94
+    //% blockGap=20 
+    //% color="#8FBC8F"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function KeyBoard_Touch(value: touch_pin): boolean {
+        let a = 0;
+        let b = 0;
+        let c = 0;
+        let temp = 0;
+        pins.i2cWriteNumber(0x50, 8, NumberFormat.UInt8BE, false);
+        a = pins.i2cReadNumber(0x50, NumberFormat.UInt8BE, true);
+        b = pins.i2cReadNumber(0x50, NumberFormat.UInt8BE, false);
+        c = (b << 8) | a;
+        if ((c & temp) != 0) {
+            c = c & temp;
+        } else {
+            switch (value) {
+                case touch_pin.None:
+                    return (c & touch.None) == 0;
+                    break;
+                case touch_pin.P2:
+                    return (c & touch.C) == 0x0001;
+                    break;
+                case touch_pin.P5:
+                    return (c & touch.D) == 0x0002;
+                    break;
+                case touch_pin.P8:
+                    return (c & touch.E) == 0x0004;
+                    break;
+                case touch_pin.P11:
+                    return (c & touch.F) == 0x0008;
+                    break;
+                case touch_pin.P12:
+                    return (c & touch.G) == 0x0010;
+                    break;
+                case touch_pin.P13:
+                    return (c & touch.A) == 0x0020;
+                    break;
+                case touch_pin.P14:
+                    return (c & touch.B) == 0x0040;
+                    break;
+                case touch_pin.P15:
+                    return (c & touch.CH) == 0x0080;
+                    break;
+                default:
+                    break;
+            }
         }
+        return false;
     }
-    return false;
+
 }
